f2533c39549021d86c729a80fcaeb02027866438
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-04-19 19:08:19 +0100

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -1,48 +1,84 @@
 #include "main.h"
 
 /**
-* print_d - Print decimal (Base 10) digits (%d)
-* @ar_list: Store the value of arguments
-* Return: Counter print numbers
-*/
-int print_d(va_list ar_list)
+ * print_int - prints an integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_int(va_list l, flags_t *f)
 {
-	unsigned int num_Absolute, aux_Num, count_Zero, count;
-	int numbers;
+	int n = va_arg(l, int);
+	int res = count_digit(n);
 
-	count = 0;
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
+}
 
-	numbers = va_arg(ar_list, int);
+/**
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int print_unsigned(va_list l, flags_t *f)
+{
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
-	if (numbers < 0)
+	(void)f;
+	return (_puts(str));
+}
+
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
 	{
-		num_Absolute = (numbers * -1);
-		count += _putchar(45);
+		_putchar('-');
+		n1 = -n;
 	}
 	else
-		num_Absolute = numbers;
+		n1 = n;
 
-	aux_Num = num_Absolute;
-	count_Zero = 1;
-	while (aux_Num > 9)
-	{
-		aux_Num /= 10;
-		count_Zero *= 10;
-	}
-
-	while (count_Zero >= 1)
-	{
-		count += _putchar(((num_Absolute / count_Zero) % 10) + '0');
-		count_Zero /= 10;
-	}
-	return (count);
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
+
 /**
-* print_i - Print the integers (%i)
-* @ar_list: Store list numbers
-* Return: Number print
-*/
-int print_i(va_list ar_list)
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
 {
-	return (print_d(ar_list));
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
 }
